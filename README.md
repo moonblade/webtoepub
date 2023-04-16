76f5e16306ba59bcdaa6945822353149a5ace70c
moonblade <moonblade168@gmail.com>
2023-04-16 08:48:03 +0530

    updated to work

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+Web To Epub
+
 Use rss feeds to automatically get the latest chapters in an online series, convert them to epub and send it to kindle
--- webtoepub.py
+++ webtoepub.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 import feedparser
 import pickle
 import time
 import subprocess
+import ssl
 
 feeds = [{
     "url": "https://wanderinginn.com/feed/"
@@ -21,18 +22,20 @@ class WebToEpub:
     def __init__(self,feedObj):
         self.completedUrls = set([])
         self.getData()
+        ssl._create_default_https_context = ssl._create_unverified_context
         self.feed = feedparser.parse(feedObj["url"])
 
     def convert(self):
         for entry in self.feed.entries[::-1]:
             if entry.link not in self.completedUrls and "Protected:" not in entry.title:
                 self.epub(entry.link, self.feed.feed.title + " - " + entry.title + "(" + time.strftime("%Y-%m-%d", entry.published_parsed) + ")")
-                # print(entry.title, entry.link, entry.published)
 
     def epub(self, url, title):
         print("Converting: ", title)
         subprocess.check_call('percollate epub ' + url + ' -o "output/' + title +  '.epub" -t "' + title + '"', shell=True)
-        subprocess.check_call('book "output/' + title + '.epub"')
+        print("Sending: ", title)
+        subprocess.check_call('echo book | mutt -s "' + title + '" -a "output/' + title + '.epub" -- mnishamk95@kindle.com', shell=True)
+        print("---")
         self.complete(url)
 
     def complete(self, url):
@@ -47,7 +50,6 @@ class WebToEpub:
         try:
             with open("completed.db", "rb") as data:
                 self.completedUrls = pickle.load(data)
-                self.completedUrls.remove("https://wanderinginn.com/2023/04/09/9-41-pt-1/")
         except Exception:
             with open("completed.db", "wb") as data:
                 pickle.dump(self.completedUrls, data)
@@ -55,4 +57,3 @@ class WebToEpub:
 
 
 main = Feeds(feeds)
-webToEpub = WebToEpub(feeds[0])
