972770028c6837dbecf16be277cdcf5d4d9f15dc
Mohammed Nisham <moonblade@orion.local>
2023-04-16 07:43:55 +0530

    update to book command

---
+++ README.md
@@ -0,0 +1,1 @@
+Use rss feeds to automatically get the latest chapters in an online series, convert them to epub and send it to kindle
--- webtoepub.py
+++ webtoepub.py
@@ -2,9 +2,9 @@
 import feedparser
 import pickle
 import time
+import subprocess
 
 feeds = [{
-    "title": "The Wandering Inn",
     "url": "https://wanderinginn.com/feed/"
 }]
 
@@ -19,31 +19,38 @@ class Feeds:
 
 class WebToEpub:
     def __init__(self,feedObj):
-        self.compltedUrls = set([])
+        self.completedUrls = set([])
         self.getData()
         self.feed = feedparser.parse(feedObj["url"])
 
     def convert(self):
         for entry in self.feed.entries[::-1]:
-            if entry not in self.compltedUrls and "Protected:" not in entry.title:
+            if entry.link not in self.completedUrls and "Protected:" not in entry.title:
                 self.epub(entry.link, self.feed.feed.title + " - " + entry.title + "(" + time.strftime("%Y-%m-%d", entry.published_parsed) + ")")
                 # print(entry.title, entry.link, entry.published)
 
     def epub(self, url, title):
         print("Converting: ", title)
+        subprocess.check_call('percollate epub ' + url + ' -o "output/' + title +  '.epub" -t "' + title + '"', shell=True)
+        subprocess.check_call('book "output/' + title + '.epub"')
+        self.complete(url)
 
+    def complete(self, url):
+        self.completedUrls.add(url)
+        self.saveData()
 
     def saveData(self):
         with open("completed.db", "wb") as data:
-            pickle.dump(self.compltedUrls, data)
+            pickle.dump(self.completedUrls, data)
 
     def getData(self):
         try:
             with open("completed.db", "rb") as data:
-                self.compltedUrls = pickle.load(data)
+                self.completedUrls = pickle.load(data)
+                self.completedUrls.remove("https://wanderinginn.com/2023/04/09/9-41-pt-1/")
         except Exception:
             with open("completed.db", "wb") as data:
-                pickle.dump(self.compltedUrls, data)
+                pickle.dump(self.completedUrls, data)
 
 
 
